7a1755998ced6f0ef914111fb76aebd4a223fb4a
Tom Cernera <tomcernera@gmail.com>
2019-07-02 19:43:25 -0400

    Updated colors

--- client/src/components/Square.jsx
+++ client/src/components/Square.jsx
@@ -1,18 +1,18 @@
 import React from 'react';
 
 const displayFalse = {
-  margin: '100px',
   background: 'gray'
 };
 
 const displayTrue = {
-  margin: '100px',
-  background: 'blue'
+  background: 'lightBlue',
+  textAlign : 'center'
 };
 
 
 var Square = props => (
-  <td onClick={()=>(props.handleClick(props.row,props.col))} style={props.display ? displayTrue : displayFalse}>
+  <td  width="40" height="40" onClick={()=>(props.handleClick(props.row,props.col))} style={props.display ? displayTrue : displayFalse}>
+  {props.display ? props.value === 0 ? '' : props.value : ''}
   </td>
 )
 
