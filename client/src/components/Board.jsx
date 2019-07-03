9ff3600669ccd395867302876e7ce0b7547f1fa2
Tom Cernera <tomcernera@gmail.com>
2019-07-03 15:58:56 -0400

    Add high score table

--- client/src/components/Board.jsx
+++ client/src/components/Board.jsx
@@ -3,25 +3,6 @@ import Square from './Square.jsx'
 
 
 var Board = props => (
-<<<<<<< HEAD
-  <table width="400" height="400">
-    <tbody>
-      {props.valueBoard.map((row,i) => {
-        return (
-        <tr key={i}>
-          {row.map((col, j) => {
-          return <Square  key={[i,j]}
-                          row={i} col={j} 
-                          handleClick={props.handleClick} 
-                          value={props.valueBoard[i][j]} 
-                          display={props.displayBoard[i][j]}/>
-          })}
-        </tr>
-        )
-      })}
-    </tbody>
-  </table>
-=======
   <React.Fragment>
     <table width="400" height="400">
       <tbody>
@@ -48,7 +29,6 @@ var Board = props => (
       <button onClick={props.handleSameOptions}>Play Same Setup</button>
     </div>
   </React.Fragment>
->>>>>>> 939a073c7e346e8f3500ff8049ee46bf65f0684e
 )
 
 
