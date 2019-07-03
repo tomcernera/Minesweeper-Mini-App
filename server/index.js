5e0bc072764ec5ea983dbd3ddc62894e71753ece
lancerutkin <lance.rutkin@gmail.com>
2019-07-03 15:24:45 -0400

    Implement high score submission

--- server/index.js
+++ server/index.js
@@ -1,12 +1,21 @@
 const express = require('express')
 const path = require('path')
 const app = express()
+const bodyParser = require('body-parser');
+const { createScore } = require('../db/index.js');
 
-app.use(express.static(path.join(__dirname,'../client/dist')))
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname,'../client/dist')));
 
-
+app.post('/scores', (req, res) => {
+  let score = req.body;
+  console.log(score);
+  // createScore(score).then(() => {
+    res.sendStatus(201);
+  // });
+});
 
 app.listen(3019, (err)=> {
   if(err) console.log(err)
-  else console.log('Listening on port3019')
+  else console.log('Listening on port 3019')
 })
